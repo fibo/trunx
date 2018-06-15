ef8da6be1df13ad9f1bad54437aabd7c65d034b8
fibo <casati_gianluca@yahoo.it>
2018-06-16 00:42:27 +0200

    customize top left corner

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,15 @@
 import { configure } from '@storybook/react'
 
+import { setOptions } from '@storybook/addon-options'
+
 import 'bulma/css/bulma.css'
+
+import { name, homepage } from '../package.json'
+
+setOptions({
+  name,
+  url: homepage
+})
 
 function loadStories () {
   require('../src/elements/stories.js')
