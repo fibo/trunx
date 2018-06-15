b5d9721d74f09a7a38365240f14c20d3cba8aa31
fibo <casati_gianluca@yahoo.it>
2018-06-16 01:54:15 +0200

    added build script

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+})
+
+var _Button = require('./src/elements/Button')
+
+var _Button2 = _interopRequireDefault(_Button)
+
+function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
+
+exports.default = {
+  Button: _Button2.default
+}
