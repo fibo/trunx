74761cdf0eb6e06c152d2f4e0ceb1301d002086b
Gianluca Casati <fibo@users.noreply.github.com>
2018-06-16 11:28:36 +0200

    added Notification element

--- src/elements/stories.js
+++ src/elements/stories.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react'
+
 import { storiesOf } from '@storybook/react'
+
 import Button from './Button.jsx'
+import Notification from './Notification'
 
 storiesOf('elements/Button', module)
   .add('basics', () => (
@@ -13,5 +16,19 @@ storiesOf('elements/Button', module)
       <Button dark>Dark</Button>
       <Button black>Black</Button>
       <Button text>Text</Button>
+    </Fragment>
+  ))
+
+storiesOf('elements/Notification', module)
+  .add('basics', () => (
+    <Notification>Notification</Notification>
+  ))
+  .add('colors', () => (
+    <Fragment>
+      <Notification white>White</Notification>
+      <Notification light>Light</Notification>
+      <Notification dark>Dark</Notification>
+      <Notification black>Black</Notification>
+      <Notification text>Text</Notification>
     </Fragment>
   ))
