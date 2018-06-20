f83118248c85bbc9c4b7ed25c54418fde5c1e676
Gianluca Casati <fibo@users.noreply.github.com>
2018-06-20 15:13:42 +0200

    started porting to TypeScript :metal:

--- src/elements/Title.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Title extends Component {
-  render () {
-    return (
-      <p className='title'>this.props.children</p>
-    )
-  }
-}
