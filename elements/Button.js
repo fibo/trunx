419100a90b2fd6e8d1c3b68ce9ea8ba175da082d
fibo <casati_gianluca@yahoo.it>
2018-06-29 17:55:50 +0200

    passing props

--- elements/Button.js
+++ elements/Button.js
@@ -8,6 +8,23 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var classname = require("classname");
 var React = require("react");
@@ -17,7 +34,7 @@ var Button = (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Button.prototype.render = function () {
-        var _a = this.props, href = _a.href, isActive = _a.isActive, isBlack = _a.isBlack, isDanger = _a.isDanger, isDark = _a.isDark, isDisabled = _a.isDisabled, isFocused = _a.isFocused, isLarge = _a.isLarge, isLight = _a.isLight, isLink = _a.isLink, isInfo = _a.isInfo, isInverted = _a.isInverted, isLoading = _a.isLoading, isOutlined = _a.isOutlined, isPrimary = _a.isPrimary, isRounded = _a.isRounded, isStatic = _a.isStatic, isSuccess = _a.isSuccess, isText = _a.isText, isWarning = _a.isWarning, isWhite = _a.isWhite, onClick = _a.onClick;
+        var _a = this.props, disabled = _a.disabled, href = _a.href, isActive = _a.isActive, isBlack = _a.isBlack, isDanger = _a.isDanger, isDark = _a.isDark, isDisabled = _a.isDisabled, isFocused = _a.isFocused, isLarge = _a.isLarge, isLight = _a.isLight, isLink = _a.isLink, isInfo = _a.isInfo, isInverted = _a.isInverted, isLoading = _a.isLoading, isOutlined = _a.isOutlined, isPrimary = _a.isPrimary, isRounded = _a.isRounded, isStatic = _a.isStatic, isSuccess = _a.isSuccess, isText = _a.isText, isWarning = _a.isWarning, isWhite = _a.isWhite, onClick = _a.onClick, props = __rest(_a, ["disabled", "href", "isActive", "isBlack", "isDanger", "isDark", "isDisabled", "isFocused", "isLarge", "isLight", "isLink", "isInfo", "isInverted", "isLoading", "isOutlined", "isPrimary", "isRounded", "isStatic", "isSuccess", "isText", "isWarning", "isWhite", "onClick"]);
         var className = classname(['button'], {
             'is-active': isActive,
             'is-black': isBlack,
@@ -40,7 +57,7 @@ var Button = (function (_super) {
             'is-warning': isWarning,
             'is-white': isWhite,
         });
-        return href ? (React.createElement("a", { className: className, href: href }, this.props.children)) : (React.createElement("button", { className: className, onClick: onClick }, this.props.children));
+        return href ? (React.createElement("a", __assign({ className: className, href: href }, props), this.props.children)) : (React.createElement("button", __assign({ className: className, disabled: disabled, onClick: onClick }, props), this.props.children));
     };
     return Button;
 }(React.Component));
