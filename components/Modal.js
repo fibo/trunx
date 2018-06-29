9335f79e3f10e222ac84758d5b3f853d7f97445e
Gianluca Casati <fibo@users.noreply.github.com>
2018-06-29 18:42:51 +0200

    nested modal components

--- components/Modal.js
+++ components/Modal.js
@@ -11,6 +11,84 @@ var __extends = (this && this.__extends) || (function () {
 Object.defineProperty(exports, "__esModule", { value: true });
 var classname = require("classname");
 var React = require("react");
+var ModalCardBody = (function (_super) {
+    __extends(ModalCardBody, _super);
+    function ModalCardBody() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ModalCardBody.prototype.render = function () {
+        return (React.createElement("section", { className: "modal-card-body" }, this.props.children));
+    };
+    return ModalCardBody;
+}(React.Component));
+var ModalCardFoot = (function (_super) {
+    __extends(ModalCardFoot, _super);
+    function ModalCardFoot() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ModalCardFoot.prototype.render = function () {
+        return (React.createElement("footer", { className: "modal-card-foot" }, this.props.children));
+    };
+    return ModalCardFoot;
+}(React.Component));
+var ModalCardHead = (function (_super) {
+    __extends(ModalCardHead, _super);
+    function ModalCardHead() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ModalCardHead.prototype.render = function () {
+        return (React.createElement("header", { className: "modal-card-head" }, this.props.children));
+    };
+    return ModalCardHead;
+}(React.Component));
+var ModalCardTitle = (function (_super) {
+    __extends(ModalCardTitle, _super);
+    function ModalCardTitle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ModalCardTitle.prototype.render = function () {
+        return (React.createElement("p", { className: "modal-card-title" }, this.props.children));
+    };
+    return ModalCardTitle;
+}(React.Component));
+var ModalCard = (function (_super) {
+    __extends(ModalCard, _super);
+    function ModalCard() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ModalCard.prototype.render = function () {
+        return (React.createElement("div", { className: "modal-card" }, this.props.children));
+    };
+    ModalCard.Body = ModalCardBody;
+    ModalCard.Foot = ModalCardFoot;
+    ModalCard.Head = ModalCardHead;
+    ModalCard.Title = ModalCardTitle;
+    return ModalCard;
+}(React.Component));
+var ModalClose = (function (_super) {
+    __extends(ModalClose, _super);
+    function ModalClose() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ModalClose.prototype.render = function () {
+        var isLarge = this.props.isLarge;
+        var className = classname(['modal-close'], {
+            'is-large': isLarge,
+        });
+        return (React.createElement("button", { "aria-label": "close", className: className }));
+    };
+    return ModalClose;
+}(React.Component));
+var ModalContent = (function (_super) {
+    __extends(ModalContent, _super);
+    function ModalContent() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ModalContent.prototype.render = function () {
+        return (React.createElement("div", { className: "modal-content" }, this.props.children));
+    };
+    return ModalContent;
+}(React.Component));
 var Modal = (function (_super) {
     __extends(Modal, _super);
     function Modal() {
@@ -25,6 +103,9 @@ var Modal = (function (_super) {
             React.createElement("div", { className: "modal-background" }),
             this.props.children));
     };
+    Modal.Card = ModalCard;
+    Modal.Close = ModalClose;
+    Modal.Content = ModalContent;
     return Modal;
 }(React.Component));
 exports.default = Modal;
