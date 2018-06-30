11bfc642c81d94c3d847408526e25b243ba9d915
fibo <casati_gianluca@yahoo.it>
2018-07-01 00:17:10 +0200

    added some typography helpers

--- src/elements/Content.tsx
+++ src/elements/Content.tsx
@@ -2,6 +2,10 @@ import * as classname from 'classname'
 import * as React from 'react'
 
 interface Props {
+  hasTextCentered?: boolean
+  hasTextJustified?: boolean
+  hasTextLeft?: boolean
+  hasTextRight?: boolean
   isLarge?: boolean
   isMedium?: boolean
   isSmall?: boolean
@@ -10,12 +14,17 @@ interface Props {
 export default class Content extends React.Component<Props> {
   public render() {
     const {
+      hasTextCentered,
       isLarge,
       isMedium,
       isSmall
     } = this.props
 
     const className = classname(['content'], {
+      'has-text-centered': hasTextCentered,
+      'has-text-justified': hasTextJustified,
+      'has-text-left': hasTextLeft,
+      'has-text-right': hasTextRight,
       'is-large': isLarge,
       'is-medium': isMedium,
       'is-small': isSmall
