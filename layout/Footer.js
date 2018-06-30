cd8f8ff1fa81442fe6184bcb20c2f1e749e2ba40
Gianluca Casati <fibo@users.noreply.github.com>
2018-07-01 00:26:55 +0200

    added Footer

---
+++ layout/Footer.js
@@ -0,0 +1,23 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var Footer = (function (_super) {
+    __extends(Footer, _super);
+    function Footer() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Footer.prototype.render = function () {
+        return (React.createElement("footer", { className: "footer" }, this.props.children));
+    };
+    return Footer;
+}(React.Component));
+exports.default = Footer;
