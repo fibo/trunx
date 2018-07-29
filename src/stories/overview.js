3ec7dc78f8d0a49d73ebbd81ec3dd001939d4801
fibo <casati_gianluca@yahoo.it>
2018-07-29 16:42:55 +0000

    more on custom build

--- src/stories/overview.js
+++ src/stories/overview.js
@@ -32,10 +32,36 @@ storiesOf('Overview', module)
           </pre>
 
           <p>
-            It is also required to add Bulma CSS. You can use a CDN, see <a href='https://bulma.io/documentation/'>Bulma documentation</a> or a custom Bulma theme, checkout <a href='https://jenil.github.io/bulmaswatch/'>Bulmaswatch</a> but the good new is that it is easy to customize your Bulma build to create your own theme.
+            It is also required to add Bulma CSS. You can use a CDN, see <a href='https://bulma.io/documentation/'>Bulma documentation</a> or a custom Bulma theme, checkout <a href='https://jenil.github.io/bulmaswatch/'>Bulmaswatch</a>.
+            but the good new is that it is easy to customize your Bulma build to create your own theme.
           </p>
 
           <p>
+            It is also easy to add Bulma via webpack. Install Bulma using npm.
+          </p>
+
+          <pre>
+            <code>
+              {indent`
+                npm install bulma
+              `}
+            </code>
+          </pre>
+
+          <p>
+            Then in your entry JS file or wherever, import Bulma CSS
+          </p>
+
+          <pre>
+            <code>
+              {indent`
+                import 'bulma/css/bulma.css'
+              `}
+            </code>
+          </pre>
+
+          <p>
+            The good new is that you can use Sass to customize your Bulma build to create your own theme.
             Install Bulma and Sass using npm.
           </p>
 
@@ -47,6 +73,51 @@ storiesOf('Overview', module)
               `}
             </code>
           </pre>
+
+          <p>
+            Create, for example, a <em>_colors.scss</em> file like the following
+          </p>
+
+          <pre>
+            <code>
+              {indent`
+                $azure: hsl(180, 100%, 97%);
+                $limegreen: hsl(120, 60%, 50%);
+                $orangered: hsl(16, 100%, 50%);
+                $skyblue: hsl(197, 71%, 73%);
+                $steelblue: hsl(207, 44%, 49%);
+
+                $body-background-color: $azure;
+
+                $primary: $skyblue;
+
+                $info: $steelblue;
+                $success: $limegreen;
+                $warning: hsl(48, 89%, 60%);
+                $danger: $orangered;
+              `}
+            </code>
+          </pre>
+
+          <p>
+            In your entry Sass file import first customizations and then Bulma.
+          </p>
+
+          <pre>
+            <code>
+              {indent`
+                /* Import custom variables first. */
+                @import "colors";
+                /* Import all bulma modules. */
+                @import "bulma/bulma";
+              `}
+            </code>
+          </pre>
+
+          <p>
+            Note that you can also tweek and import individual Bulma modules, checkout <a href='https://bulma.io/documentation/'>official Bulma documentation</a>.
+          </p>
+
           <hr />
 
           <Title is4>
