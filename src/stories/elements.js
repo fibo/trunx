edaf3e8af5dd7eed46b2c5c85808dd5792716613
fibo <casati_gianluca@yahoo.it>
2018-07-29 16:27:20 +0000

    documented packages needed to customize bulma

--- src/stories/elements.js
+++ src/stories/elements.js
@@ -277,6 +277,35 @@ storiesOf('Elements', module)
 
           <hr />
 
+          <p>
+            When you can't use the CSS classes you want, or when you just want to directly use HTML tags, use <code>Content</code> as container. It can handle almost any HTML tag:
+          </p>
+
+          <ul>
+            <li>
+              <code>{'<p>'}</code> paragraphs
+            </li>
+
+            <li>
+              <code>{'<ul>'}</code> <code>{'<ol>'}</code> <code>{'<dl>'}</code> lists
+            </li>
+
+            <li>
+              <code>{'<h1>'}</code> to <code>{'<h6>'}</code> headings
+            </li>
+
+            <li>
+              <code>{'<blockquote>'}</code> quotes
+            </li>
+
+            <li>
+              <code>{'<em>'}</code> and <code>{'<strong>'}</code>
+            </li>
+
+            <li>
+              <code>{'<table>'}</code> <code>{'<tr>'}</code> <code>{'<th>'}</code> <code>{'<td>'}</code> tables
+            </li>
+          </ul>
         </Content>
       </Container>
     </Section>
