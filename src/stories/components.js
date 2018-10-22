1433466278eee4834df9394bc0f228fd6985709e
fibo <casati_gianluca@yahoo.it>
2018-10-23 00:19:40 +0200

    ModalCardExample code

--- src/stories/components.js
+++ src/stories/components.js
@@ -579,6 +579,64 @@ storiesOf('Components', module)
           </ul>
 
           <ModalCardExample />
+
+          <Code language='jsx'>
+            {indent`
+              class ModalCardExample extends Component {
+                constructor (props) {
+                  super(props)
+
+                  this.state = {
+                    modalIsActive: false
+                  }
+
+                  this.closeModal = this.closeModal.bind(this)
+                  this.openModal = this.openModal.bind(this)
+                }
+
+                closeModal () {
+                  this.setState({ modalIsActive: false })
+                }
+
+                openModal () {
+                  this.setState({ modalIsActive: true })
+                }
+
+                render () {
+                  const {
+                    modalIsActive
+                  } = this.state
+
+                  return (
+                    <Fragment>
+                      <Button
+                        isLarge
+                        isPrimary
+                        onClick={this.openModal}
+                      >Launch example modal</Button>
+                      <Modal isActive={modalIsActive}>
+                        <Modal.Background onClick={this.closeModal} />
+                        <Modal.Card>
+                          <Modal.Card.Head>
+                            <Modal.Card.Title>Modal title</Modal.Card.Title>
+
+                            <Delete onClick={this.closeModal} />
+                          </Modal.Card.Head>
+                          <Modal.Card.Body>
+                            body
+                          </Modal.Card.Body>
+                          <Modal.Card.Foot>
+                            <Button>Cancel</Button>
+                            <Button isSuccess>Save changes</Button>
+                          </Modal.Card.Foot>
+                        </Modal.Card>
+                      </Modal>
+                    </Fragment>
+                  )
+                }
+              }
+            `}
+          </Code>
         </Content>
       </Container>
     </Section>
