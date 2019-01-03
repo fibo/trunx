d888eaa0b8b11737110fa7d5d14b00ae659147e3
fibo <casati_gianluca@yahoo.it>
2019-01-03 23:18:42 +0100

    added inputRef

--- src/component/Checkbox.tsx
+++ src/component/Checkbox.tsx
@@ -2,18 +2,21 @@ import * as React from "react"
 
 interface ICheckboxProps {
   disabled?: boolean
+  inputRef?: React.RefObject<HTMLInputElement>
 }
 
 export default class Checkbox extends React.Component<ICheckboxProps> {
   render() {
     const {
       disabled,
+      inputRef,
     } = this.props
 
     return (
       <label className="checkbox">
         <input
           disabled={disabled}
+          ref={inputRef}
           type="checkbox"
         />
 
