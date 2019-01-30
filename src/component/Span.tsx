cf3023fa8bf11981f2b11558c05f4b30856bf445
fibo <casati_gianluca@yahoo.it>
2019-01-30 17:12:38 +0100

    added Span

--- src/component/Span.tsx
+++ src/component/Span.tsx
@@ -8,17 +8,18 @@ import {
   textColorHelpersPropsToClassenames,
 } from "./modifiers"
 
-interface IStrongProps extends IBackgroundColorHelpersProps,
-                               ITextColorHelpersProps {}
+interface ISpanProps extends IBackgroundColorHelpersProps,
+                             ITextColorHelpersProps {}
 
-export default class Strong extends React.Component<IStrongProps> {
+export default class Span extends React.Component<ISpanProps> {
   render() {
     const className = classnames(
+      backgroundColorHelpersPropsToClassenames(this.props),
       textColorHelpersPropsToClassenames(this.props),
     )
 
     return (
-      <strong className={className}>{this.props.children}</strong>
+      <span className={className}>{this.props.children}</span>
     )
   }
 }
