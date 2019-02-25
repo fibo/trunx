b38cbf393819161c8049fa3e7cc81dfef5dfb845
fibo <casati_gianluca@yahoo.it>
2019-02-25 23:01:47 +0100

    fixed message style

--- src/stories/components.js
+++ src/stories/components.js
@@ -747,44 +747,45 @@ storiesOf('Components', module)
             Colored <b>message</b> blocks, to emphasize part of your page
           </Subtitle>
 
-          <Meta colors sizes />
-
-          <hr />
-
-          <Columns>
-            <Column isHalf>
-              <Message>
-                <Message.Header>
-                  <p>Hello World</p>
-
-                  <Delete />
-                </Message.Header>
-
-                <Message.Body>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-                </Message.Body>
-              </Message>
-            </Column>
-
-            <Column isHalf>
-              <Code language='jsx'>
-                {indent`
-                  <Message>
-                    <Message.Header>
-                      <p>Hello World</p>
-
-                      <Delete />
-                    </Message.Header>
-
-                    <Message.Body>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-                    </Message.Body>
-                  </Message>
-                `}
-              </Code>
-            </Column>
-          </Columns>
         </Content>
+
+        <Meta colors sizes />
+
+        <hr />
+
+        <Columns>
+          <Column isHalf>
+            <Message>
+              <Message.Header>
+                <p>Hello World</p>
+
+                <Delete />
+              </Message.Header>
+
+              <Message.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </Message.Body>
+            </Message>
+          </Column>
+
+          <Column isHalf>
+            <Code language='jsx'>
+              {indent`
+                <Message>
+                  <Message.Header>
+                    <p>Hello World</p>
+
+                    <Delete />
+                  </Message.Header>
+
+                  <Message.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+                  </Message.Body>
+                </Message>
+              `}
+            </Code>
+          </Column>
+        </Columns>
       </Container>
     </Section>
   ))
