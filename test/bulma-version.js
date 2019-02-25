ad25741ecef74a10dbcfa75ca2e584050af4f4cf
Gianluca Casati <fibo@users.noreply.github.com>
2019-02-25 21:13:01 +0100

    sync bulma and trunx major versions, closes #6

---
+++ test/bulma-version.js
@@ -0,0 +1,8 @@
+const assert = require('assert')
+
+const { version, dependencies } = require('../package.json')
+
+const trunxMajorVersion = version.split('.')[0]
+const bulmaMajorVersion = dependencies.bulma.substring(1).split('.')[0]
+
+assert.equal(bulmaMajorVersion, trunxMajorVersion, 'Bulma and Trunx major versions are the same')
