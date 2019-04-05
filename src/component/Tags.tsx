706735e3711a91f4e2c577826e74ab6a282d9280
fibo <casati_gianluca@yahoo.it>
2019-04-05 18:52:03 +0200

    tag sizes

--- src/component/Tags.tsx
+++ src/component/Tags.tsx
@@ -2,16 +2,22 @@ import * as classnames from "classnames"
 import * as React from "react"
 
 interface ITagsProps {
+  areLarge?: boolean
+  areMedium?: boolean
   hasAddons?: boolean
 }
 
 export default class Tags extends React.Component<ITagsProps> {
   render() {
     const {
+      areLarge,
+      areMedium,
       hasAddons,
     } = this.props
 
     const className = classnames("tags", {
+      "are-large": areLarge,
+      "are-medium": areMedium,
       "has-addons": hasAddons,
     })
 
