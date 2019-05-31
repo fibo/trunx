daaf0da3e5ee7adb8becce3c679d9ddf32126166
Gianluca Casati <fibo@users.noreply.github.com>
2019-05-31 17:03:31 +0200

    bump version

--- component/Column.js
+++ component/Column.js
@@ -8,7 +8,7 @@ var Column = (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Column.prototype.render = function () {
-        var _a = this.props, is1 = _a.is1, is2 = _a.is2, is3 = _a.is3, is4 = _a.is4, is5 = _a.is5, is6 = _a.is6, is7 = _a.is7, is8 = _a.is8, is9 = _a.is9, is10 = _a.is10, is11 = _a.is11, is12 = _a.is12, isFourFifths = _a.isFourFifths, isHalf = _a.isHalf, isNarrow = _a.isNarrow, isNarrowDesktop = _a.isNarrowDesktop, isNarrowMobile = _a.isNarrowMobile, isNarrowTablet = _a.isNarrowTablet, isNarrowTouch = _a.isNarrowTouch, isNarrowWidescreen = _a.isNarrowWidescreen, isNarrowFullhd = _a.isNarrowFullhd, isOffset1 = _a.isOffset1, isOffset2 = _a.isOffset2, isOffset3 = _a.isOffset3, isOffset4 = _a.isOffset4, isOffset5 = _a.isOffset5, isOffset6 = _a.isOffset6, isOffset7 = _a.isOffset7, isOffset8 = _a.isOffset8, isOffset9 = _a.isOffset9, isOffset10 = _a.isOffset10, isOffset11 = _a.isOffset11, isOffset12 = _a.isOffset12, isOneFifth = _a.isOneFifth, isOneQuarter = _a.isOneQuarter, isOneThird = _a.isOneThird, isThreeFifths = _a.isThreeFifths, isThreeQuarters = _a.isThreeQuarters, isTwoFifths = _a.isTwoFifths, isTwoQuarters = _a.isTwoQuarters, isTwoThirds = _a.isTwoThirds;
+        var _a = this.props, is1 = _a.is1, is2 = _a.is2, is3 = _a.is3, is4 = _a.is4, is5 = _a.is5, is6 = _a.is6, is7 = _a.is7, is8 = _a.is8, is9 = _a.is9, is10 = _a.is10, is11 = _a.is11, is12 = _a.is12, isCentered = _a.isCentered, isFourFifths = _a.isFourFifths, isHalf = _a.isHalf, isNarrow = _a.isNarrow, isNarrowDesktop = _a.isNarrowDesktop, isNarrowMobile = _a.isNarrowMobile, isNarrowTablet = _a.isNarrowTablet, isNarrowTouch = _a.isNarrowTouch, isNarrowWidescreen = _a.isNarrowWidescreen, isNarrowFullhd = _a.isNarrowFullhd, isOffset1 = _a.isOffset1, isOffset2 = _a.isOffset2, isOffset3 = _a.isOffset3, isOffset4 = _a.isOffset4, isOffset5 = _a.isOffset5, isOffset6 = _a.isOffset6, isOffset7 = _a.isOffset7, isOffset8 = _a.isOffset8, isOffset9 = _a.isOffset9, isOffset10 = _a.isOffset10, isOffset11 = _a.isOffset11, isOffset12 = _a.isOffset12, isOneFifth = _a.isOneFifth, isOneQuarter = _a.isOneQuarter, isOneThird = _a.isOneThird, isThreeFifths = _a.isThreeFifths, isThreeQuarters = _a.isThreeQuarters, isTwoFifths = _a.isTwoFifths, isTwoQuarters = _a.isTwoQuarters, isTwoThirds = _a.isTwoThirds;
         var className = classnames("column", {
             "is-1": is1,
             "is-2": is2,
@@ -22,6 +22,7 @@ var Column = (function (_super) {
             "is-10": is10,
             "is-11": is11,
             "is-12": is12,
+            "is-centered": isCentered,
             "is-four-fifths": isFourFifths,
             "is-half": isHalf,
             "is-narrow": isNarrow,
