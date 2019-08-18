94850132a395e5eac4e0918db89dd22c8ff14b01
fibo <casati_gianluca@yahoo.it>
2019-08-18 17:11:07 +0200

    added navbar to documentation website

--- src/docs/Root.js
+++ src/docs/Root.js
@@ -5,13 +5,19 @@ import {
   Switch
 } from 'react-router-dom'
 
-import Overview from './pages/Overview'
+import Overview from './pages/Overview.js'
+import ModifiersSyntax from './pages/ModifiersSyntax.js'
+import Nav from './components/Nav.js'
 
 export default function Root () {
   return (
     <BrowserRouter>
+      <Route component={Nav} path='*' />
+
       <Switch>
         <Route component={Overview} exact path='/' />
+
+        <Route component={ModifiersSyntax} exact path='/modifiers/syntax' />
       </Switch>
     </BrowserRouter>
   )
