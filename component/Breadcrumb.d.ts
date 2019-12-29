f22943aed97a425b88d8cf4f25e8df67b740c982
fibo <casati_gianluca@yahoo.it>
2019-12-29 15:50:10 +0100

    Breadcrumb.item

---
+++ component/Breadcrumb.d.ts
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { ISizeProps } from "./modifiers";
+interface IBreadcrumbProps extends ISizeProps, React.HTMLAttributes<HTMLElement> {
+    hasArrowSeparator?: boolean;
+    hasBulletSeparator?: boolean;
+    hasDotSeparator?: boolean;
+    hasSuccedesSeparator?: boolean;
+    isCentered?: boolean;
+    isRight?: boolean;
+}
+interface IBreadcrumbItemProps extends React.LiHTMLAttributes<HTMLLIElement> {
+    isActive?: boolean;
+}
+declare class BreadcrumbItem extends React.Component<IBreadcrumbItemProps> {
+    render(): JSX.Element;
+}
+export default class Breadcrumb extends React.Component<IBreadcrumbProps> {
+    static Item: typeof BreadcrumbItem;
+    render(): JSX.Element;
+}
+export {};
---
+++ component/Breadcrumb.js
@@ -0,0 +1,45 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var classnames = require("classnames");
+var React = require("react");
+var modifiers_1 = require("./modifiers");
+var BreadcrumbItem = (function (_super) {
+    tslib_1.__extends(BreadcrumbItem, _super);
+    function BreadcrumbItem() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    BreadcrumbItem.prototype.render = function () {
+        var _a = this.props, children = _a.children, isActive = _a.isActive, props = tslib_1.__rest(_a, ["children", "isActive"]);
+        var className = classnames({
+            "is-active": isActive,
+        });
+        return (React.createElement("li", tslib_1.__assign({}, props, { className: className }), children));
+    };
+    return BreadcrumbItem;
+}(React.Component));
+var Breadcrumb = (function (_super) {
+    tslib_1.__extends(Breadcrumb, _super);
+    function Breadcrumb() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Breadcrumb.prototype.render = function () {
+        var _a = this.props, children = _a.children, hasArrowSeparator = _a.hasArrowSeparator, hasBulletSeparator = _a.hasBulletSeparator, hasDotSeparator = _a.hasDotSeparator, hasSuccedesSeparator = _a.hasSuccedesSeparator, isCentered = _a.isCentered, isLarge = _a.isLarge, isMedium = _a.isMedium, isRight = _a.isRight, isSmall = _a.isSmall;
+        var className = classnames("breadcrumb", {
+            "has-arrow-separator": hasArrowSeparator,
+            "has-bullet-separator": hasBulletSeparator,
+            "has-dot-separator": hasDotSeparator,
+            "has-succedes-separator": hasSuccedesSeparator,
+            "is-centered": isCentered,
+            "is-right": isRight,
+        }, modifiers_1.sizePropsToClassnames({
+            isLarge: isLarge,
+            isMedium: isMedium,
+            isSmall: isSmall,
+        }));
+        return (React.createElement("nav", { "aria-label": "breadcrumbs", className: className },
+            React.createElement("ul", null, children)));
+    };
+    Breadcrumb.Item = BreadcrumbItem;
+    return Breadcrumb;
+}(React.Component));
+exports.default = Breadcrumb;
