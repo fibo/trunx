ec63ff4cd35e30196753e9ffcdc4cf79d46c4889
fibo <casati_gianluca@yahoo.it>
2020-03-22 11:18:00 +0100

    generate sitemap

--- test/bulma-version.js
+++ test/bulma-version.js
@@ -1,8 +1,19 @@
-const assert = require('assert')
+import assert from 'assert'
+import path from 'path'
+import readFile from 'read-file-utf8'
+import { fileURLToPath } from 'url'
 
-const { version, dependencies } = require('../package.json')
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-const trunxMajorVersion = version.split('.')[0]
-const bulmaMajorVersion = dependencies.bulma.substring(1).split('.')[0]
+const packageJsonFilepath = path.join(__dirname, '..', 'package.json')
 
-assert.strict.equal(bulmaMajorVersion, trunxMajorVersion, 'Bulma and Trunx major versions are the same')
+async function testBulmaVersion () {
+  const { dependencies, version } = await readFile(packageJsonFilepath).then(JSON.parse)
+
+  const trunxMajorVersion = version.split('.')[0]
+  const bulmaMajorVersion = dependencies.bulma.substring(1).split('.')[0]
+
+  assert.strict.equal(bulmaMajorVersion, trunxMajorVersion, 'Bulma and Trunx major versions are the same')
+}
+
+testBulmaVersion()
