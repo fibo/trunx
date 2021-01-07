d6e4e1fb3c72f0c29f78a15cfb3a2cd2dc81cfe0
fibo <casati_gianluca@yahoo.it>
2021-01-07 16:26:44 +0100

    fix Breadcrumb docs page

--- component/Li.js
+++ component/Li.js
@@ -1,8 +1,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Li = void 0;
 var tslib_1 = require("tslib");
+var classnames = require("classnames");
 var React = require("react");
-var renderElement_1 = require("./renderElement");
+var classNames_1 = require("./classNames");
 var Li = (function (_super) {
     tslib_1.__extends(Li, _super);
     function Li() {
@@ -17,8 +18,8 @@ var Li = (function (_super) {
         if (this.state.hasError) {
             return null;
         }
-        var _a = this.props, isActive = _a.isActive, props = tslib_1.__rest(_a, ["isActive"]);
-        return renderElement_1.renderElement('li', props, undefined, { isActive: isActive });
+        var _a = this.props, children = _a.children, className = _a.className, isActive = _a.isActive, props = tslib_1.__rest(_a, ["children", "className", "isActive"]);
+        return (React.createElement("li", tslib_1.__assign({ className: classnames(className, classNames_1.trunxPropsToClassnamesObject({ isActive: isActive })) }, props), children));
     };
     return Li;
 }(React.Component));
