15dbcc22f6bfacac14c11bff1dce716821edae9f
fibo <fibo@users.noreply.github.com>
2023-02-01 19:51:12 +0100

    npm workspaces: packages/trunx webapps/trunx-docs

--- component/renderElement.d.ts
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import { BulmaClassName, TrunxProps } from './classNames';
-export declare function renderElement(type: string, elementProps: {
-    [prop: string]: any;
-}, bulmaClassName?: BulmaClassName, trunxProps?: TrunxProps): React.ReactNode;
---
+++ webapps/trunx-docs/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import { Container, Section, Title } from 'trunx'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>trunx</title>
+        <meta
+          name='description'
+          content='Super Saiyan React components, son of awesome Bulma'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <main>
+        <Section>
+          <Container>
+            <Title>Getting started with Trunx</Title>
+          </Container>
+        </Section>
+      </main>
+    </>
+  )
+}
