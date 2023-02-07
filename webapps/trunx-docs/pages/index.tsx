d1b76445f1771291dd84f95f190c966a35e90b80
shrutivtu <shrutisaxena1596@gmail.com>
2023-02-07 23:21:22 +0530

    feat: review comments address

--- webapps/trunx-docs/pages/index.tsx
+++ webapps/trunx-docs/pages/index.tsx
@@ -1,16 +1,13 @@
 import Head from 'next/head'
 import { Container, Section, Title } from 'trunx'
-import Nav from '../components/Nav';
+import { Nav } from '../components/Nav'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>trunx</title>
-        <meta
-          name='description'
-          content='Super Saiyan React components, son of awesome Bulma'
-        />
+        <meta name='description' content='Super Saiyan React components, son of awesome Bulma' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
