1186ce4c9432ab004b954cbdbf2d37a63f1e1668
shrutivtu <shrutisaxena1596@gmail.com>
2023-02-07 23:21:22 +0530

    feat: review comments address

--- webapps/trunx-docs/components/Nav.tsx
+++ webapps/trunx-docs/components/Nav.tsx
@@ -1,71 +1,58 @@
-import React, { useState } from 'react';
-import { Navbar } from 'trunx';
-import Image from 'next/image';
+import { useEffect, useState } from 'react'
+import { Navbar } from 'trunx'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { route } from '../routes/routes';
-import { navContents } from '../helpers/navContent';
+import { route } from '../routes/routes'
+import { navContents } from '../helpers/navContent'
 
-const Nav = (): JSX.Element => {
-    const router = useRouter();
+export const Nav = (): JSX.Element => {
+  const router = useRouter()
 
-    const { pathname } = router;
+  const { pathname } = router
 
-    const [expanded, setExpanded] = useState<boolean>(false);
-    const [redirect, setRedirect] = useState<string | null>(null);
+  const [expanded, setExpanded] = useState<boolean>(false)
+  const [redirect, setRedirect] = useState('')
 
-    const redirectTo = (wantedPathname: string) => (): void => {
-        if (pathname !== wantedPathname) {
-          setRedirect(wantedPathname)
-        }
+  useEffect(() => {
+    if (redirect !== '') {
+      router.push(redirect)
     }
-    const onClickBurger = (): void => {
-        setExpanded(!expanded)
-    }
+  }, [redirect])
 
-    if (typeof redirect === 'string') {
-        router.push(redirect);
+  const redirectTo = (wantedPathname: string) => (): void => {
+    if (pathname !== wantedPathname) {
+      setRedirect(wantedPathname)
     }
-    return (
-        <Navbar isPrimary>
-            <Navbar.Brand>
-                <Navbar.Item 
-                    onClick={redirectTo(route.home)}
-                >
-                    <Image 
-                        src='/assets/trunx-logotype-white.png' 
-                        alt="me" 
-                        width="100" 
-                        height="100" 
-                    />
-                </Navbar.Item>
-                <Navbar.Burger 
-                    isActive={expanded} 
-                    onClick={onClickBurger} 
-                />
-            </Navbar.Brand>
-            <Navbar.Menu>
-                <Navbar.Start>
-                    {navContents.map(({ label, items }, i) => (
-                        <Navbar.Item hasDropdown isHoverable key={i}>
-                        <Navbar.Link>{label}</Navbar.Link>
+  }
+  const onClickBurger = (): void => {
+    setExpanded(!expanded)
+  }
 
-                        <Navbar.Dropdown>
-                            {items.map(({ label, route }, i) => (
-                            <Navbar.Item
-                                isActive={route === pathname}
-                                key={i}
-                                onClick={redirectTo(route)}
-                            >
-                                {label}
-                            </Navbar.Item>
-                            ))}
-                        </Navbar.Dropdown>
-                        </Navbar.Item>
+  return (
+    <Navbar isPrimary>
+      <Navbar.Brand>
+        <Navbar.Item onClick={redirectTo(route.home)}>
+          <Image src='/assets/trunx-logotype-white.png' alt='me' width='100' height='100' />
+        </Navbar.Item>
+        <Navbar.Burger isActive={expanded} onClick={onClickBurger} />
+      </Navbar.Brand>
+      <Navbar.Menu>
+        <Navbar.Start>
+          {navContents.map(({ label, items }, i) => (
+            <Navbar.Item hasDropdown isHoverable key={i}>
+              <Navbar.Link>{label}</Navbar.Link>
+
+              <Navbar.Dropdown>
+                {items.map(({ label, route }, i) => (
+                  <Navbar.Item isActive={route === pathname} key={i} onClick={redirectTo(route)}>
+                    {label}
+                  </Navbar.Item>
                 ))}
-                </Navbar.Start>
-            </Navbar.Menu>
-        </Navbar>
-    )
+              </Navbar.Dropdown>
+            </Navbar.Item>
+          ))}
+        </Navbar.Start>
+      </Navbar.Menu>
+    </Navbar>
+  )
 }
-
-export default Nav;
