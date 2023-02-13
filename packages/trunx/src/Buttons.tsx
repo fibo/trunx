150263238eeb25c3fe53685c7400e549f5797871
fibo <fibo@users.noreply.github.com>
2023-02-13 17:39:00 +0100

    port Buttons component to React.FC

--- packages/trunx/src/Buttons.tsx
+++ packages/trunx/src/Buttons.tsx
@@ -1,44 +1,15 @@
-import classnames from 'classnames'
-import * as React from 'react'
+import { FC, HTMLAttributes, PropsWithChildren, useMemo } from 'react'
+import { classNames } from './classNames.js'
+import { bulma } from './bulma.js'
+import { CenteredProp, SizeProp } from './modifiers.js'
+import { centeredClassName, pluralSizeClassName } from './helpers.js'
 
-import { ErrorBoundaryProps } from './ErrorBoundary.js'
-import { bulmaClassName, trunxPropsToClassnamesObject } from './classNames.js'
-import { HelpersProps, extractModifiersProps, modifierPropsToClassnamesObject } from './modifiers.js'
+type Props = HTMLAttributes<HTMLDivElement> & CenteredProp & SizeProp
 
-export interface ButtonsProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    ErrorBoundaryProps,
-    HelpersProps {
-  areLarge?: boolean
-  areMedium?: boolean
-  areSmall?: boolean
-}
-
-export class Buttons extends React.Component<ButtonsProps> {
-  static getDerivedStateFromError() {
-    return { hasError: true }
-  }
-
-  state = { hasError: false }
-
-  render() {
-    const [modifiersProps, { areLarge, areMedium, areSmall, children, className, fallbackUI, ...props }] =
-      extractModifiersProps(this.props)
-
-    if (this.state.hasError) return fallbackUI
-
-    return (
-      <div
-        className={classnames(
-          bulmaClassName.buttons,
-          className,
-          modifierPropsToClassnamesObject(modifiersProps),
-          trunxPropsToClassnamesObject({ areLarge, areMedium, areSmall })
-        )}
-        {...props}
-      >
-        {children}
-      </div>
-    )
-  }
+export const Buttons: FC<PropsWithChildren<Props>> = ({ children, className, centered, size }) => {
+  const _className = useMemo(
+    () => classNames(bulma('buttons', centeredClassName(centered), pluralSizeClassName(size)), className),
+    [className, size]
+  )
+  return <div className={_className}>{children}</div>
 }
