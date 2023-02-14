a12e50dce0de332672619e20982d5c4595583944
fibo <fibo@users.noreply.github.com>
2023-02-14 13:50:05 +0100

    add components/ folder, port Navbar

--- npm-scripts/generate_BulmaClassName_type/postcss.config.cjs
+++ npm-scripts/generate_BulmaClassName_type/postcss.config.cjs
@@ -1,6 +1,5 @@
 // This config is used by `generate_BulmaClassName_type` npm script.
 //
-// The name of this file is measleading, should be renamed but postcss --config flag was not working.
 // This current implementation has a great value added, by now it is worth to keep it as is.
 //
 // The ideal solution would be to create a postcss plugin starting from postcss-ts-classnames.
