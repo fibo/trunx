c6a4658731800d3eb53277ae9ceffa8312c787a3
fibo <fibo@users.noreply.github.com>
2023-02-17 18:08:00 +0100

    port Tile

--- packages/trunx/src/components/Buttons.tsx
+++ packages/trunx/src/components/Buttons.tsx
@@ -12,10 +12,20 @@ export type ButtonsProps = HTMLAttributes<HTMLDivElement> &
   SizeModifierProp<Size> &
   Pick<CommonModifierProps, 'isCentered'>
 
-export const Buttons: FC<PropsWithChildren<ButtonsProps>> = ({ children, className, isCentered, size }) => {
+export const Buttons: FC<PropsWithChildren<ButtonsProps>> = ({
+  children,
+  className,
+  isCentered,
+  size,
+  ...props
+}) => {
   const _className = useMemo(
     () => classNames('buttons', modifier({ isCentered }), pluralSizeClassName(size), className),
     [className, isCentered, size]
   )
-  return <div className={_className}>{children}</div>
+  return (
+    <div className={_className} {...props}>
+      {children}
+    </div>
+  )
 }
