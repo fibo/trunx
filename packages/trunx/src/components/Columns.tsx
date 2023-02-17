89fb7019d47660ce71d63b65647a2aa32b3f0133
fibo <fibo@users.noreply.github.com>
2023-02-17 12:25:17 +0100

    add Label, Control and base Column, Columns

--- packages/trunx/src/components/Columns.tsx
+++ packages/trunx/src/components/Columns.tsx
@@ -1,37 +1,57 @@
-import * as React from 'react'
+import { FC, HTMLAttributes, PropsWithChildren, useMemo } from 'react'
+import { classNames } from '../classNames.js'
+import {
+  Breakpoint,
+  BreakpointProps,
+  CommonModifierProps,
+  responsivenessClassName,
+  modifier,
+} from '../modifiers/index.js'
 
-import { ErrorBoundaryProps } from './ErrorBoundary.js'
-import { bulmaClassName } from './classNames.js'
-import { HelpersProps } from './modifiers.js'
-import { renderElement } from './renderElement.js'
+type Gap = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 
-export interface ColumnsProps
-  extends React.HTMLAttributes<HTMLDivElement>,
-    ErrorBoundaryProps,
-    HelpersProps {
-  isDesktop?: boolean
-  isGapless?: boolean
-  isMobile?: boolean
-  isMultiline?: boolean
-}
+export type ColumnsProps = HTMLAttributes<HTMLDivElement> &
+  Pick<BreakpointProps, 'isMobile' | 'isDesktop'> &
+  Pick<CommonModifierProps, 'isCentered'> &
+  Partial<{
+    gap: Gap | { [key in Breakpoint]: Gap }
+    isGapless: boolean
+    isNarrow: boolean
+    isMultiline: boolean
+    isVariable: boolean
+    isVcentered: boolean
+  }>
 
-export class Columns extends React.Component<ColumnsProps> {
-  static getDerivedStateFromError() {
-    return { hasError: true }
-  }
-
-  state = { hasError: false }
-
-  render(): React.ReactNode {
-    const { fallbackUI, isDesktop, isGapless, isMobile, isMultiline, ...props } = this.props
-
-    if (this.state.hasError) return fallbackUI
-
-    return renderElement('div', props, bulmaClassName.columns, {
-      isDesktop,
-      isGapless,
-      isMobile,
-      isMultiline,
-    })
-  }
+export const Columns: FC<PropsWithChildren<ColumnsProps>> = ({
+  className,
+  children,
+  gap,
+  isCentered,
+  isDesktop,
+  isGapless,
+  isMobile,
+  isMultiline,
+  isNarrow,
+  isVariable,
+  isVcentered,
+}) => {
+  const _className = useMemo(
+    () =>
+      classNames(
+        'columns',
+        responsivenessClassName({ isMobile, isDesktop }),
+        modifier({ isCentered }),
+        typeof gap === 'number' ? `is-${gap}` : '',
+        {
+          'is-gapless': isGapless,
+          'is-multiline': isMultiline,
+          'is-narrow': isNarrow,
+          'is-variable': isVariable,
+          'is-vcentered': isVcentered,
+        },
+        className
+      ),
+    [className, gap, isCentered, isMobile, isMultiline, isNarrow, isDesktop, isVcentered]
+  )
+  return <div className={_className}>{children}</div>
 }
