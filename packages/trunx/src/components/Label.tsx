5c258e65f035e1fbfb80070ed958d2f0bce9a1fa
fibo <fibo@users.noreply.github.com>
2023-02-17 12:25:17 +0100

    add Label, Control and base Column, Columns

--- packages/trunx/src/components/Label.tsx
+++ packages/trunx/src/components/Label.tsx
@@ -1,28 +1,13 @@
-import * as React from 'react'
+import { FC, LabelHTMLAttributes, PropsWithChildren, useMemo } from 'react'
+import { classNames } from '../classNames.js'
+import { SizeModifierProp, sizeClassName } from '../modifiers/index.js'
 
-import { ErrorBoundaryProps } from './ErrorBoundary.js'
-import { bulmaClassName } from './classNames.js'
-import { HelpersProps, SizeProps } from './modifiers.js'
-import { renderElement } from './renderElement.js'
+export type LabelProps = LabelHTMLAttributes<HTMLLabelElement> & SizeModifierProp
 
-export interface LabelProps
-  extends React.LabelHTMLAttributes<HTMLLabelElement>,
-    ErrorBoundaryProps,
-    HelpersProps,
-    SizeProps {}
-
-export class Label extends React.Component<LabelProps> {
-  static getDerivedStateFromError() {
-    return { hasError: true }
-  }
-
-  state = { hasError: false }
-
-  render(): React.ReactNode {
-    const { fallbackUI, ...props } = this.props
-
-    if (this.state.hasError) return fallbackUI
-
-    return renderElement('label', props, bulmaClassName.label)
-  }
+export const Label: FC<PropsWithChildren<LabelProps>> = ({ children, className, size }) => {
+  const _className = useMemo(
+    () => classNames('label', sizeClassName(size), className),
+    [className, size, size]
+  )
+  return <label className={_className}>{children}</label>
 }
