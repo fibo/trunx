437af5135a579f7dcfc4c8bde4b2d0ac604ed3da
fibo <fibo@users.noreply.github.com>
2023-02-18 16:24:45 +0100

    port Progress

--- packages/trunx/src/components/Progress.tsx
+++ packages/trunx/src/components/Progress.tsx
@@ -1,30 +1,21 @@
-import * as React from 'react'
+import { ProgressHTMLAttributes, FC, useMemo } from 'react'
+import { classNames } from '../classNames.js'
+import {
+  ColorModifierProp,
+  MainColor,
+  SizeModifierProp,
+  colorClassName,
+  sizeClassName,
+} from '../modifiers/index.js'
 
-import { ErrorBoundaryProps } from './ErrorBoundary.js'
-import { bulmaClassName } from './classNames.js'
-import { HelpersProps, MainColorsProps, ShadeColorsProps, SizeProps } from './modifiers.js'
-import { renderElement } from './renderElement.js'
+export type ProgressProps = ProgressHTMLAttributes<HTMLProgressElement> &
+  ColorModifierProp<MainColor> &
+  SizeModifierProp
 
-export interface ProgressProps
-  extends React.ProgressHTMLAttributes<HTMLProgressElement>,
-    ErrorBoundaryProps,
-    HelpersProps,
-    MainColorsProps,
-    ShadeColorsProps,
-    SizeProps {}
-
-export class Progress extends React.Component<ProgressProps> {
-  static getDerivedStateFromError() {
-    return { hasError: true }
-  }
-
-  state = { hasError: false }
-
-  render(): React.ReactNode {
-    const { fallbackUI, ...props } = this.props
-
-    if (this.state.hasError) return fallbackUI
-
-    return renderElement('progress', props, bulmaClassName.progress)
-  }
+export const Progress: FC<ProgressProps> = ({ className, color, size, ...props }) => {
+  const _className = useMemo(
+    () => classNames('progress', colorClassName(color), sizeClassName(size), className),
+    [className, color, size]
+  )
+  return <progress className={_className} {...props} />
 }
--- packages/trunx/src/modifiers/size.ts
+++ packages/trunx/src/modifiers/size.ts
@@ -1,15 +1,13 @@
 import { BulmaClassName } from '../bulma.js'
 
-export type Size = 'small' | 'medium' | 'large'
+export type Size = 'small' | 'medium' | 'large' | 'normal'
 
 export type SizeModifierProp<S extends Size = Size> = Partial<{ size: S }>
 
 export const pluralSizeClassName = (
-  size: Size | undefined
-): Extract<BulmaClassName, 'are-small' | 'are-medium' | 'are-large'> | undefined =>
-  size ? `are-${size}` : undefined
+  size: Exclude<Size, 'normal'> | undefined
+): Extract<BulmaClassName, `are-${typeof size}`> | undefined => (size ? `are-${size}` : undefined)
 
 export const sizeClassName = (
   size: Size | undefined
-): Extract<BulmaClassName, 'is-small' | 'is-medium' | 'is-large'> | undefined =>
-  size ? `is-${size}` : undefined
+): Extract<BulmaClassName, `is-${typeof size}`> | undefined => (size ? `is-${size}` : undefined)
