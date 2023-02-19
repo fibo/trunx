b71c51077dc4387401dfcaa98ec256c3a9cb5ce4
fibo <fibo@users.noreply.github.com>
2023-02-19 14:22:52 +0100

    port Tag and Radio

--- packages/trunx/src/components/Radio.tsx
+++ packages/trunx/src/components/Radio.tsx
@@ -1,41 +1,12 @@
-import classnames from 'classnames'
-import * as React from 'react'
+import { FC, InputHTMLAttributes, PropsWithChildren } from 'react'
 
-import { ErrorBoundaryProps } from './ErrorBoundary.js'
-import { bulmaClassName } from './classNames.js'
-import { HelpersProps, extractModifiersProps, modifierPropsToClassnamesObject } from './modifiers.js'
+export type RadioProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>
 
-export interface RadioProps
-  extends React.InputHTMLAttributes<HTMLInputElement>,
-    ErrorBoundaryProps,
-    HelpersProps {}
-
-export class Radio extends React.Component<RadioProps> {
-  static getDerivedStateFromError() {
-    return { hasError: true }
-  }
-
-  state = { hasError: false }
-
-  render(): React.ReactNode {
-    const [modifiersProps, { children, className, fallbackUI, ...props }] = extractModifiersProps(
-      this.props
-    )
-
-    if (this.state.hasError) return fallbackUI
-
-    return (
-      <label
-        className={classnames(
-          bulmaClassName.radio,
-          className,
-          modifierPropsToClassnamesObject(modifiersProps)
-        )}
-      >
-        <input {...props} type='radio' />
-
-        {children}
-      </label>
-    )
-  }
+export const Radio: FC<PropsWithChildren<RadioProps>> = ({ children, className, ...props }) => {
+  return (
+    <label className="radio">
+      <input type="radio" {...props} />
+      {children}
+    </label>
+  )
 }
