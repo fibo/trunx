f8ae6de5f3b4512d9bc26209fc8904284d991f8a
fibo <fibo@users.noreply.github.com>
2023-03-03 12:37:00 +0100

    add page layout components

--- webapps/trunx-docs/pages/index.tsx
+++ webapps/trunx-docs/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import { Container, Section, bulma } from 'trunx'
-import { Nav, MessageComp, Installation, CodeRequirement, WorkExample } from '@/components'
+import { Message, bulma } from 'trunx'
+import { Nav, Installation, CodeRequirement, PageContent, PageSection, WorkExample } from '@/components'
 
 export default function Home() {
   return (
@@ -11,31 +11,35 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main>
         <Nav />
-        <Section>
-          <Container>
-            <h2 className={bulma('title', 'is-2')}>Getting started with Trunx</h2>
-            <p>
+
+        <PageContent
+          title="Getting started with Trunx"
+          subtitle={
+            <>
               <em>Trunx</em> is an open source collection of <em>React</em> components built on top of{' '}
-              <em>Bulma</em> CSS framework and implemented with <em>TypeScript</em>. GitHub repo here:{' '}
-              <a href="https://github.com/fibo/trunx">github.com/fibo/trunx</a>.
+              <em>Bulma</em>, implemented with <em>TypeScript</em>
+            </>
+          }
+        >
+          <PageSection>
+            <p className={bulma('block')}>
+              GitHub repo here: <a href="https://github.com/fibo/trunx">github.com/fibo/trunx</a>.
             </p>
-            <MessageComp>
+
+            <Message color="info">
               <p>Almost all bulma components are implemented, yet not documented here.</p>
-            </MessageComp>
+            </Message>
+          </PageSection>
 
-            <Installation />
+          <Installation />
 
-            <hr />
+          <CodeRequirement />
 
-            <CodeRequirement />
-
-            <hr />
-
-            <WorkExample />
-          </Container>
-        </Section>
+          <WorkExample />
+        </PageContent>
       </main>
     </>
   )
