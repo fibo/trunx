009887d0642723548a9c133c467e99119ac90e17
fibo <fibo@users.noreply.github.com>
2023-03-03 18:29:43 +0100

    remove modifiers pages

--- webapps/trunx-docs/helpers/navContent.ts
+++ webapps/trunx-docs/helpers/navContent.ts
@@ -1,64 +1,57 @@
-import { route } from '../routes/routes'
+import { route } from "../routes/routes"
 
 export const navContents = [
   {
-    label: 'Modifiers',
+    label: "Columns",
     items: [
-      { label: 'Syntax', route: route.modifiers.syntax },
-      { label: 'Color helpers', route: route.modifiers.colorHelpers },
-    ],
-  },
-  {
-    label: 'Columns',
-    items: [
-      { label: 'Basics', route: route.columns.basics },
-      { label: 'Sizes', route: route.columns.sizes },
+      { label: "Basics", route: route.columns.basics },
+      { label: "Sizes", route: route.columns.sizes },
       {
-        label: 'Responsiveness',
+        label: "Responsiveness",
         route: route.columns.responsiveness,
       },
-      { label: 'Nesting', route: route.columns.nesting },
+      { label: "Nesting", route: route.columns.nesting },
     ],
   },
   {
-    label: 'Layout',
+    label: "Layout",
     items: [
-      { label: 'Container', route: route.layout.container },
-      { label: 'Level', route: route.layout.level },
-      { label: 'Media Object', route: route.layout.mediaObject },
+      { label: "Container", route: route.layout.container },
+      { label: "Level", route: route.layout.level },
+      { label: "Media Object", route: route.layout.mediaObject },
       // TODO fix hero foot
       // { label: 'Hero', route: route.layout.hero },
-      { label: 'Footer', route: route.layout.footer },
-      { label: 'Tiles', route: route.layout.tiles },
+      { label: "Footer", route: route.layout.footer },
+      { label: "Tiles", route: route.layout.tiles },
     ],
   },
   {
-    label: 'Form',
+    label: "Form",
     items: [
-      { label: 'General', route: route.form.general },
-      { label: 'Input', route: route.form.input },
-      { label: 'Textarea', route: route.form.textarea },
-      { label: 'Radio', route: route.form.radio },
+      { label: "General", route: route.form.general },
+      { label: "Input", route: route.form.input },
+      { label: "Textarea", route: route.form.textarea },
+      { label: "Radio", route: route.form.radio },
     ],
   },
   {
-    label: 'Elements',
+    label: "Elements",
     items: [
-      { label: 'Box', route: route.elements.box },
-      { label: 'Button', route: route.elements.button },
-      { label: 'Content', route: route.elements.content },
-      { label: 'Delete', route: route.elements.delete },
-      { label: 'Tag', route: route.elements.tag },
-      { label: 'Title', route: route.elements.title },
+      { label: "Box", route: route.elements.box },
+      { label: "Button", route: route.elements.button },
+      { label: "Content", route: route.elements.content },
+      { label: "Delete", route: route.elements.delete },
+      { label: "Tag", route: route.elements.tag },
+      { label: "Title", route: route.elements.title },
     ],
   },
   {
-    label: 'Components',
+    label: "Components",
     items: [
-      { label: 'Breadcrumb', route: route.components.breadcrumb },
-      { label: 'Navbar', route: route.components.navbar },
-      { label: 'Message', route: route.components.message },
-      { label: 'Modal', route: route.components.modal },
+      { label: "Breadcrumb", route: route.components.breadcrumb },
+      { label: "Navbar", route: route.components.navbar },
+      { label: "Message", route: route.components.message },
+      { label: "Modal", route: route.components.modal },
     ],
   },
 ]
