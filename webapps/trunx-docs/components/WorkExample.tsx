1a1414ed57698b6a5efde2afa58d501d729b6c82
fibo <fibo@users.noreply.github.com>
2023-03-03 13:23:00 +0100

    remove subpath exports

--- webapps/trunx-docs/components/WorkExample.tsx
+++ webapps/trunx-docs/components/WorkExample.tsx
@@ -1,13 +1,20 @@
-import { Button, Column, Columns, Message } from 'trunx'
-// import { Button } from 'trunx/components/Button'
-import { Code } from './Code'
-import { PageSection } from './PageSection'
-import { indent } from '@/helpers/utils/indent'
+import { Button, Column, Columns, Message, bulma } from "trunx"
+import { Code } from "./Code"
+import { PageSection } from "./PageSection"
+import { indent } from "@/helpers/utils/indent"
 
 export const WorkExample = () => {
   return (
     <PageSection title="How it works">
-      <p>Trunx React components wrap Bulma CSS classes.</p>
+      <p>
+        Trunx React components wrap Bulma CSS classes. For example, import <code>Button</code> component.
+      </p>
+
+      <Code>
+        {indent`
+          import { Button } from "trunx";
+        `}
+      </Code>
 
       <Columns>
         <Column isNarrow>
@@ -23,11 +30,11 @@ export const WorkExample = () => {
 
           <Code>
             {indent`
-          <Button
-            color="primary"
-            size="large"
-          >Push me</Button>
-        `}
+              <Button
+                color="primary"
+                size="large"
+              >Push me</Button>
+            `}
           </Code>
         </Column>
 
@@ -43,64 +50,31 @@ export const WorkExample = () => {
       </Columns>
 
       <p>
-        To import components, the following syntaxes are supported. For example to import the{' '}
-        <code>Button</code> component you can use both ES6 or CommonJS syntaxes.
+        You can also use <code>bulma</code> helper if some feature or HTML tag is not implemented by Trunx.
       </p>
 
       <Code>
         {indent`
-          import { Button } from 'trunx'
+          import { bulma } from "trunx";
         `}
       </Code>
 
-      <Code>
-        {indent`
-          const { Button } = require('trunx')
+      <Columns>
+        <Column>
+          You are <strong className={bulma("has-text-success")}>successful</strong>!
+        </Column>
+        <Column>
+          <Code>
+            {indent`
+          You are <strong className={bulma("has-text-success")}>successful</strong>!
         `}
-      </Code>
+          </Code>
+        </Column>
+      </Columns>
 
-      <p>
-        It is also possible to import components directly. If you do not have <em>Tree shaking</em> setup in
-        your build, the following imports will create lighter bundles <b>out of the box</b>.
-      </p>
-
-      <Code>
-        {indent`
-          import Button from 'trunx/component/Button'
-        `}
-      </Code>
-
-      <Code>
-        {indent`
-          const Button = require('trunx/component/Button')
-        `}
-      </Code>
-
-      <p>
-        Note that you can still use Bulma directly if some feature or HTML tag is not implemented by Trunx,
-        for example
-      </p>
-
-      <Code>
-        {indent`
-          You are <strong className='has-text-success'>successful</strong>!
-        `}
-      </Code>
-
-      <Message color="info" header="Features implemented by every component">
-        <>
-          <p>Notice also that:</p>
-          <ul>
-            <li>
-              Every <em>prop</em> in a Trunx component is <b>optional</b>.
-            </li>
-
-            <li>
-              Every Trunx component accepts a <code>className</code> prop which will be appended to Bulma
-              classes in order to customize the component style.
-            </li>
-          </ul>
-        </>
+      <Message color="info">
+        Many Trunx component accept a <code>className</code> prop which will be appended to Bulma classes in
+        order to customize the component style.
       </Message>
     </PageSection>
   )
