d1b3e2dbd5a0262004bb23e5e1e8c414b01a35b5
fibo <fibo@users.noreply.github.com>
2023-04-20 18:00:07 +0200

    fix spacing, it is a Partial

--- packages/trunx/src/modifiers/layout.ts
+++ packages/trunx/src/modifiers/layout.ts
@@ -1,47 +1,53 @@
-import { classNames } from '../classNames.js'
+import { classNames } from "../classNames.js"
 
-export type Alignment = 'centered' | 'left' | 'right'
+export type Alignment = "centered" | "left" | "right"
 
 export type FlexAlignContent =
-  | 'flex-start'
-  | 'flex-end'
-  | 'center'
-  | 'space-between'
-  | 'space-around'
-  | 'space-evenly'
-  | 'stretch'
-  | 'start'
-  | 'end'
-  | 'baseline'
+  | "flex-start"
+  | "flex-end"
+  | "center"
+  | "space-between"
+  | "space-around"
+  | "space-evenly"
+  | "stretch"
+  | "start"
+  | "end"
+  | "baseline"
 
 export type FlexAlignItems =
-  | 'stretch'
-  | 'flex-start'
-  | 'flex-end'
-  | 'center'
-  | 'baseline'
-  | 'start'
-  | 'end'
-  | 'self-start'
-  | 'self-end'
+  | "stretch"
+  | "flex-start"
+  | "flex-end"
+  | "center"
+  | "baseline"
+  | "start"
+  | "end"
+  | "self-start"
+  | "self-end"
 
-export type FlexAlignSelf = 'auto' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch'
+export type FlexAlignSelf =
+  | "auto"
+  | "flex-start"
+  | "flex-end"
+  | "center"
+  | "baseline"
+  | "stretch"
 
-export type FlexDirection = 'row' | 'row-reverse' | 'column' | 'column-reverse'
+export type FlexDirection = "row" | "row-reverse" | "column" | "column-reverse"
 
 export type FlexJustifyContent =
-  | 'flex-start'
-  | 'flex-end'
-  | 'center'
-  | 'space-between'
-  | 'space-around'
-  | 'space-evenly'
-  | 'start'
-  | 'end'
-  | 'left'
-  | 'right'
+  | "flex-start"
+  | "flex-end"
+  | "center"
+  | "space-between"
+  | "space-around"
+  | "space-evenly"
+  | "start"
+  | "end"
+  | "left"
+  | "right"
 
-export type FlexWrap = 'nowrap' | 'wrap' | 'wrap-reverse'
+export type FlexWrap = "nowrap" | "wrap" | "wrap-reverse"
 
 type FlexSpacing = 0 | 1 | 2 | 3 | 4 | 5
 
@@ -67,7 +73,7 @@ export const flexClassNames = ({
   shrink,
 }: FlexModifierProps) =>
   classNames(
-    'is-flex',
+    "is-flex",
     direction && `is-flex-direction-${direction}`,
     justify && `is-justify-content-${justify}`,
     align && `is-align-content-${align}`,
@@ -79,11 +85,25 @@ export const flexClassNames = ({
   )
 
 export type SpacingValue = 0 | 1 | 2 | 3 | 5 | 5 | 6
-export type SpacingKey = 'mx' | 'my' | 'mr' | 'ml' | 'mt' | 'mb' | 'px' | 'py' | 'pr' | 'pl' | 'pt' | 'pb'
+export type SpacingKey =
+  | "mx"
+  | "my"
+  | "mr"
+  | "ml"
+  | "mt"
+  | "mb"
+  | "px"
+  | "py"
+  | "pr"
+  | "pl"
+  | "pt"
+  | "pb"
 
-export type SpacingModifierProp = Partial<{ spacing: { [key in SpacingKey]: SpacingValue } }>
+export type SpacingModifierProp = Partial<{
+  spacing: Partial<{ [key in SpacingKey]: SpacingValue }>
+}>
 
-export const spacingClassNames = (arg: SpacingModifierProp['spacing']) =>
-  arg && !Array.isArray(arg) && typeof arg === 'object'
+export const spacingClassNames = (arg: SpacingModifierProp["spacing"]) =>
+  arg && !Array.isArray(arg) && typeof arg === "object"
     ? Object.entries(arg).map(([key, value]) => `${key}-${value}`)
     : undefined
