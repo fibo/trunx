6ab2a0022e031b65eaa7b6966296a7054905d5cd
Gianluca Casati <fibo@users.noreply.github.com>
2023-10-11 15:29:10 +0200

    add htmlFor example

--- packages/trunx/src/components/Label.tsx
+++ packages/trunx/src/components/Label.tsx
@@ -2,16 +2,20 @@ import { FC, LabelHTMLAttributes, PropsWithChildren } from "react"
 import { classNames } from "../classNames.js"
 import { SizeModifierProp, sizeClassName } from "../modifiers/index.js"
 
-export type LabelProps = LabelHTMLAttributes<HTMLLabelElement> &
+export type LabelProps = Omit<LabelHTMLAttributes<HTMLLabelElement>, "size"> &
   SizeModifierProp
 
 export const Label: FC<PropsWithChildren<LabelProps>> = ({
   children,
   className,
   size,
-  ...props,
+  ...props
 }) => {
   const _class = classNames("label", sizeClassName(size), className)
 
-  return <label className={_class} {...props}>{children}</label>
+  return (
+    <label className={_class} {...props}>
+      {children}
+    </label>
+  )
 }
