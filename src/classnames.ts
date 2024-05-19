a91af504846d78e45c284413688eb436adf70c34
Gianluca Casati <fibo@users.noreply.github.com>
2024-05-19 10:10:15 +0200

    remove workspaces

---
+++ src/classnames.ts
@@ -0,0 +1,54 @@
+export type ClassnamesArg<Classname extends string> =
+  | Classname
+  | { [key in Classname]?: unknown }
+  | ClassnamesArg<Classname>[]
+  | undefined
+
+/**
+ * Utility for conditionally joining CSS classes together.
+ *
+ * @example
+ *
+ * ```ts
+ * classnames("foo", "bar") // 'foo bar'
+ * classnames<string>("foo", ["bar"]) // 'foo bar'
+ * classnames<string>({ foo: true }, { bar: false }) // 'foo'
+ * ```
+ *
+ * @example It accepts a generic "class names" type.
+ *
+ * ```ts
+ * type T = "foo" | "bar" // my CSS classes
+ * classnames<T>("foo", "quz") // ERROR: not assignable to type ClassnamesArg<T>[]
+ * ```
+ */
+export const classnames = <T extends string>(...args: ClassnamesArg<T>[]) =>
+  args
+    .map((arg) => {
+      if (Array.isArray(arg))
+        // Recursively call `classnames` or return empty string if arg is an empty array.
+        return arg.length ? classnames(...arg) : ""
+
+      if (
+        // Here we know that `arg` is not an array.
+        // Make sure `arg` is not null,
+        arg &&
+        // and `arg` is a proper object.
+        typeof arg === "object"
+      )
+        return classnames(
+          // Map `arg` object to an array of its keys, having a truthy value.
+          Object.entries(arg).reduce(
+            (keys, keyValue) => (keyValue[1] ? keys.concat(keyValue[0]) : keys),
+            [],
+          ),
+        )
+
+      // Here `arg` should be a string or `undefined`.
+      return arg
+    })
+    .filter(
+      // Avoid more than one white space in the join below, by filtering falsy values.
+      (str) => !!str,
+    )
+    .join(" ")
--- src/version_test.ts
+++ src/version_test.ts
@@ -2,15 +2,11 @@ import assert from "node:assert/strict"
 import { test } from "node:test"
 import pkg from "../package.json" assert { type: "json" }
 
-test("@trunx/bulma version", () => {
+test("trunx version", () => {
   const { dependencies, version } = pkg
 
   const trunxMajorVersion = version.split(".")[0]
   const bulmaMajorVersion = dependencies.bulma.split(".")[0]
 
-  assert.equal(
-    bulmaMajorVersion,
-    trunxMajorVersion,
-    "Bulma and @trunx/bulma major versions are not the same"
-  )
+  assert.equal(bulmaMajorVersion, trunxMajorVersion)
 })
