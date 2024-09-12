1e850ad00cf35cb6fbca470e1548cc1990e7f4f9
Gianluca Casati <fibo@users.noreply.github.com>
2024-09-12 19:28:24 +0200

    add custom minifier

---
+++ minify.js
@@ -0,0 +1,37 @@
+import { readFile, writeFile } from "node:fs/promises"
+
+const classnamesPathname = "./dist/classnames.js"
+const componentsPathname = "./dist/react.js"
+
+const originalClassnames = await readFile(classnamesPathname, "utf8")
+const originalComponents = await readFile(componentsPathname, "utf8")
+
+const minifiedClassnames = originalClassnames
+  .replace(/\s+/g, " ")
+  .replace(/ => /g, "=>")
+  .replace(/ \./g, ".")
+  .replace(/, /g, ",")
+  .replace(/ \? /g, "?")
+  .replace(/ \}/g, "}")
+  .replace(/\{ /g, "{")
+  .replace(/ : /g, ":")
+  .replace(/ \=/g, "=")
+  .replace(/\= /g, "=")
+  .replace(/; /g, ";")
+  .replace(/if \(/g, "if(")
+
+const minifiedComponents = originalComponents
+  .replace(/\{\n/g, "{")
+  .replace(/,\s+/g, ",")
+  .replace(/\s=>\s/g, "=>")
+  .replace(/\{\s+/g, "{")
+  .replace(/\s+:/g, ":")
+  .replace(/:\s+/g, ":")
+  .replace(/\s\=/g, "=")
+  .replace(/\=\s/g, "=")
+  .replace(/\s\?\s/g, "?")
+  .replace(/\s\}/g, "}")
+  .replace(/children:children/g, "children")
+
+await writeFile(classnamesPathname, minifiedClassnames)
+await writeFile(componentsPathname, minifiedComponents)
